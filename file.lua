bffeb3277c66bc595418373a27448479260f8517
logix <daniil.romushkevich1@gmail.com>
2022-04-09 19:03:20 +0300

    update

--- file.lua
+++ file.lua
@@ -1,9 +1,57 @@
 local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
 local DragControlIer = game:GetService("ReplicatedStorage").ClientBridge.DragControlIer
+local Resize = game:GetService("ReplicatedStorage").ClientBridge.Resize --
+local Seeder = Resize.Seeder --
+
 local UserInputService = game:GetService("UserInputService")
+local HttpService = game:GetService("HttpService")
 local RunService = game:GetService("RunService")
 local GuiService = game:GetService('GuiService')
 local Debris = game:GetService("Debris")
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+
+local clientSeeds = {}
+local serverSeeds = {}
+local increments = {}
+
+Player.CharacterAdded:Connect(function()
+    wait(1)
+    for _, func in pairs(getgc()) do
+        if type(func) == "function" and islclosure(func) then
+            local source = getfenv(func).script
+            local constants = getconstants(func)
+            local nextInt = table.find(constants, "NextInteger")
+            local seed = nextInt and constants[nextInt - 1]
+            if seed and typeof(seed) == "number" then
+                clientSeeds[source.Name] = seed
+            elseif source and source:IsDescendantOf(game) then
+                if source.Name == "Paint" then
+                    increments["Color"] = function()
+                        for i, v in pairs(debug.getupvalues(func)) do
+                            if typeof(v) == "number" then
+                                local increment = v + 1
+                                debug.setupvalue(func, i, increment)
+                                return increment
+                            end
+                        end
+                    end
+                else
+                    for _, upvalue in pairs(getupvalues(func)) do
+                        if typeof(upvalue) == "Random" then
+                            serverSeeds[source.Name] = upvalue
+                            break
+                        end
+                    end
+                end
+            end
+        end
+    end
+end)
+
+function GetSeed(property)
+	return serverSeeds[property]:NextInteger(0, clientSeeds[property])
+end
 
 function GetItem(item)
     local item = Player.Backpack:FindFirstChild(item)
@@ -37,7 +85,7 @@ function Copy(part, cFrame)
         DragControlIer.Update:FireServer("ClearKey", key)
     end
     StoreItems()
-    return success and part
+    return part
 end
 
 function Drag(part, cFrame)
@@ -62,7 +110,16 @@ local function GetChildrenWhichAre(where, class)
     return rtable
 end
 
-local ColorForSelection = Color3.fromRGB(0,0,0)
+local ColorForSelection = Color3.fromHSV(0,0,0)
+
+local GUI = Instance.new("ScreenGui")
+GUI.Parent = Player.PlayerGui
+GUI.ResetOnSpawn = false
+
+local selectionFrame = Instance.new("Frame", GUI)
+selectionFrame.AnchorPoint = Vector2.new(0.5, 0.5)
+selectionFrame.Transparency = 0.7
+selectionFrame.BorderSizePixel = 0
 
 spawn(function()
 	while wait() do
@@ -70,10 +127,12 @@ spawn(function()
 		local hue = tick() % t / t
 
 		ColorForSelection = Color3.fromHSV(hue, 1, 1)
+        selectionFrame.BackgroundColor3 = ColorForSelection
 	end
 end)
 
 getgenv().Selected = {}
+getgenv().Hidden = {}
 
 spawn(function()
     while wait(0.1) do
@@ -83,19 +142,17 @@ spawn(function()
             sel.Adornee = v
             sel.Color3 = ColorForSelection
             
-            Debris:AddItem(sel, 0.101)
+            Debris:AddItem(sel, 0.11)
         end
     end
 end)
 
 local Inset = GuiService:GetGuiInset()
 local Camera = workspace.CurrentCamera
-local GUI = Instance.new("ScreenGui")
-GUI.Parent = Player.PlayerGui
 
-local selectionFrame = Instance.new("Frame", GUI)
-selectionFrame.AnchorPoint = Vector2.new(0.5, 0.5)
-selectionFrame.Transparency = 0.7
+local uicorner = Instance.new("UICorner")
+uicorner.CornerRadius = UDim.new(0, 5)
+uicorner.Parent = selectionFrame
 
 local mouseDown = false
 getgenv().SelectEquipped = false
@@ -146,7 +203,7 @@ local function Search(objs, p1, p2)
 
 	return Found
 end
-	
+
 UserInputService.InputBegan:Connect(function(input) 
 	if input.UserInputType == Enum.UserInputType.MouseButton1 and getgenv().SelectEquipped then
 		lastPos = Vector2.new(input.Position.x, input.Position.y) 
@@ -159,7 +216,7 @@ UserInputService.InputEnded:Connect(function(input)
 		
 		local pos = Vector2.new(input.Position.x, input.Position.y)
 		local result = Search(GetChildrenWhichAre(workspace, "BasePart"), To3dSpace(lastPos), To3dSpace(pos))
-		mouseDown = false; selectionFrame.Visible = false 
+		mouseDown = false; selectionFrame.Visible = false
 		
 		getgenv().Selected = {}
 		
@@ -169,6 +226,13 @@ UserInputService.InputEnded:Connect(function(input)
 	end
 end)
 
+-- Seeder.OnClientEvent:Connect(function(arg)
+-- 	seed = Random.new(arg);
+-- 	for v26 = 1, seed:NextInteger(5, 50) do
+-- 		seed:NextInteger(1, 2);
+-- 		getgenv().seed = seed
+-- 	end
+-- end) this was the 0 iq method (it worked doe)
 
 RunService.Heartbeat:Connect(function()
 	if mouseDown and getgenv().SelectEquipped then
@@ -203,6 +267,14 @@ local VIP_REQ = UI.New({
     Title = "VIP Required"
 })
 
+local Essential = UI.New({
+    Title = "Essential"
+})
+
+-- local Saving = UI.New({
+--     Title = "Saving"
+-- })
+
 local GetSelect = Main.Button({
     Text = "Get select tool",
     Callback = function()
@@ -211,27 +283,74 @@ local GetSelect = Main.Button({
         SelectTool.RequiresHandle = false
         SelectTool.Equipped:Connect(function()
             getgenv().SelectEquipped = true
-			getgenv().Selected = {}
+            getgenv().Selected = {}
         end)
         
         SelectTool.Unequipped:Connect(function()
            	getgenv().SelectEquipped = false
-			getgenv().Selected = {}
+            getgenv().Selected = {}
         end)
+    end
+})
+
+local GetSelect = Main.Button({
+    Text = "Remove selection (in case something breaks)",
+    Callback = function()
+        getgenv().Selected = {}
     end
 })
 
 local LockSelected = VIP_REQ.Button({
 	Text = "Toggle Lock Selected",
 	Callback = function()
-	    if Player.Backpack:FindFirstChild("VIP") or Player.Character:FindFirstChild("VIP") then
-    	    for i,v in pairs(getgenv().Selected) do
-    	        GetItem("VIP")
-                game:GetService("ReplicatedStorage").ClientBridge.RequestPropertyChange:InvokeServer(v, "Locked", true)
-                wait(0.1)
-    	    end
-    	    
-    	    getgenv().Selected = {}
+	    for i,v in pairs(getgenv().Selected) do
+	        GetItem("VIP")
+            game:GetService("ReplicatedStorage").ClientBridge.RequestPropertyChange:InvokeServer(v, "Locked", true)
+            wait(0.2)
 	    end
+	    
+	    getgenv().Selected = {}
+	    StoreItems()
 	end
 })
+
+Essential.Label({
+    Text = "Properties"
+})
+
+local ChangePropertyName = Essential.TextField({
+    Text = "Input Property Name"
+})
+
+local ChangePropertyValue = Essential.TextField({
+    Text = "Input Property Value"
+})
+
+local ChangeProperty = Essential.Button({
+    Text = "Change Property (Need VIP to change VIP properties)",
+    Callback = function()
+
+        GetItem("Properties")
+        GetItem("VIP")
+        
+        for i,v in pairs(getgenv().Selected) do
+            prop_value = nil
+            local success, res = pcall(function()
+                prop_value = tonumber(ChangePropertyValue:GetText())
+            end)
+            if not success then
+                prop_value = ChangePropertyValue:GetText() == "true"
+            end
+
+            pcall(function()
+                game:GetService("ReplicatedStorage").ClientBridge.RequestPropertyChange:InvokeServer(v, ChangePropertyName:GetText(), prop_value)
+            end)
+            wait(0.2)
+        end
+        
+        getgenv().Selected = {}
+        StoreItems()
+    end
+})
+
+Players:Chat("re")
